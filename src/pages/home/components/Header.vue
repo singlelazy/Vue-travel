1f199389649d549366fca758e37701ba59ab88ec
Lazy-dog <y371430933@163.com>
2018-06-05 09:02:42 +0800

    add city-router

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
           <span class="iconfont">&#xe633;</span> 
           输入城市/景点/游玩主题
         </div>
-        <div class="header-right">
-          {{this.city}}
-          <span class="iconfont arrow-icon">&#xe64a;</span>
-        </div>
+        <router-link to="/city">
+          <div class="header-right">
+            {{this.city}}
+            <span class="iconfont arrow-icon">&#xe64a;</span>
+          </div>
+        </router-link>
     </div>
 </template>
 <script>
@@ -25,7 +27,7 @@
   @import "~style/varibles.styl"
   .header
     display: flex
-    line-height: .86rem
+    line-height: $headerHeight
     background: $bgColor
     color: #fff
     .header-left
@@ -45,9 +47,11 @@
       color: #ccc
       border-radius: .1rem
     .header-right
+      display:block
       width: 1.24rem
       float: right
       text-align: center
+      color:#fff
       .arrow-icon
         font-size: .24rem
 </style>
