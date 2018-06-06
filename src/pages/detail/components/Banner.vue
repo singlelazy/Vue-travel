8963d347e924b1aed1d81990e43aa705438ddc27
Lazy-dog <y371430933@163.com>
2018-06-06 19:51:26 +0800

    add animation

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -12,16 +12,19 @@
         </div>
       </div>
     </div>
-    <common-gallery 
-      :imgs="bannerImgs" 
-      v-show="showGallery"
-      @close="handleGalleryClose"
-    ></common-gallery>   
+    <fade-animation>
+      <common-gallery 
+        :imgs="bannerImgs" 
+        v-show="showGallery"
+        @close="handleGalleryClose"
+      ></common-gallery>     
+    </fade-animation> 
   </div>
 </template>
 
 <script>
 import CommonGallery from 'common/gallery/Gallery'
+import FadeAnimation from 'common/fade/FadeAnimation'
 export default {
   name: 'DetailBanner',
   props:{
@@ -35,7 +38,8 @@ export default {
     }
   },
   components:{
-    CommonGallery
+    CommonGallery,
+    FadeAnimation
   },
   methods:{
     handleBannerClick(){
